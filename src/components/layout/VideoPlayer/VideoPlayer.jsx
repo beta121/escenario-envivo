b5404bc169487fceee1b57c02db3f0485c727471
beta121 <business@smartexperience.global>
2026-05-29 18:07:02 +0000

    add new stream and video

--- src/components/layout/VideoPlayer/VideoPlayer.jsx
+++ src/components/layout/VideoPlayer/VideoPlayer.jsx
@@ -64,7 +64,7 @@ export const VideoPlayer = ({ videoUrl, streamerData, status }) => {
               controls={isRecordedVideo}
               autoPlay={!isRecordedVideo}
               loop={!isRecordedVideo}
-              muted={!isRecordedVideo}
+              muted={false}
               autoPlay={true}
               playsInline
               onCanPlay={() => setIsLoading(false)}
